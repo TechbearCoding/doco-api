9b897a73497d718d929957595c89c5ffdbf859d3
Martins Leitarts <martins.leitarts@avalanchelabs.com>
2022-05-16 01:40:36 +0300

    Created sample post

--- mainNamspace.py
+++ mainNamspace.py
@@ -1,7 +1,11 @@
-from flask_restx import Namespace, Resource, reqparse #tiek importēti moduļi
-import sqlite3 as sqlite #import sqllite
+from flask_restx import Namespace, Resource, reqparse
+from flask import request
 
 api = Namespace('Document fill', description='Fill data')
+
+parser = reqparse.RequestParser()
+parser.add_argument('firstName', type=str, help='name')
+parser.add_argument('lastName', type=str, help='last names')
 
 
 @api.route('/check')
@@ -9,7 +13,12 @@ class Checker(Resource):
     def get(self):
         return 'It works!'
 
-# @api.route('/fill')
-# class Filler(Resource):
-#     def post()
+@api.route('/fill')
+class Filler(Resource):
+    @api.doc(parser=parser)
+    def post(self):
+        args = parser.parse_args()
+        firstName = args['firstName']
+        lastName = args['lastName'] 
+        return firstName + ' ' + lastName
 
