6631432e59d481a40f2ff77641b36187e2390fb8
Martins Leitarts <21012644+TechbearCoding@users.noreply.github.com>
2022-05-18 03:30:21 +0300

    Added code, error

--- mainNamspace.py
+++ mainNamspace.py
@@ -1,7 +1,11 @@
 from flask_restx import Namespace, Resource, reqparse
 from flask import request
+from mailmerge import MailMerge
+
 
 api = Namespace('Document fill', description='Fill data')
+template = "contract.doc"
+
 
 parser = reqparse.RequestParser()
 parser.add_argument('firstName', type=str, help='name')
@@ -20,5 +24,8 @@ class Filler(Resource):
         args = parser.parse_args()
         firstName = args['firstName']
         lastName = args['lastName'] 
+        document = MailMerge(template)
+        document.merge(Name = firstName + ' ' + lastName)
+        document.write('test-output.docx')
         return firstName + ' ' + lastName
 
