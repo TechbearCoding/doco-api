2c1df01e2c681cc29da1517fba28d060cdc0c30b
Martins Leitarts <martins.leitarts@avalanchelabs.com>
2022-05-19 22:07:47 +0300

    Data model complete

--- mainNamspace.py
+++ mainNamspace.py
@@ -9,8 +9,19 @@ template = "contract.docx"
 
 model = api.model('Resource', {
     'name': fields.String,
-    'lastName': fields.String
+    'lastName': fields.String,
+    'companyName': fields.String,
+    'regNr': fields.String,
+    'email': fields.String,
+    'phone': fields.String,
+    'address': fields.String,
+    'bank': fields.String,
+    'bankCode': fields.String,
+    'accountNr': fields.String,
+    'date': fields.String
 })
+
+# 2022.gada ___.decembrī
 
 @api.route('/check')
 class Checker(Resource):
@@ -28,6 +39,15 @@ class Filler(Resource):
 
         document = MailMerge(template)
         document.merge(Name = firstName + ' ' + lastName)
+        document.merge(companyName = item['companyName'])
+        document.merge(regNr = item['regNr'])
+        document.merge(email = item['email'])
+        document.merge(phone = item['phone'])
+        document.merge(address = item['address'])
+        document.merge(bank = item['bank'])
+        document.merge(bankCode = item['bankCode'])
+        document.merge(accountNr = item['accountNr'])
+        document.merge(date = item['date'])
         document.write('test-output.docx')
         return firstName + ' ' + lastName
 
